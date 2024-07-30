5937c6d890adb5290cd758e8b9ebe52312988196
tanvichoudharyy <ctanvi614@gmail.com>
2024-07-30 20:07:00 +0530

    fix issues

--- project/src/App.js
+++ project/src/App.js
@@ -1,19 +1,7 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom"
-import { Navbar } from './common/Navbar';
 import { Dashboard } from './components/Dashboard';
 import { Signup } from "./Signup";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-    secondary: {
-      main: '#dc004e',
-    },
-  },
-});
 
 function App() {
   return (
--- project/src/components/signup.js
+++ project/src/components/signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Signup from "./common/Signup";
 
 const Signup = () => {
   const handleSubmit = (event) => {
